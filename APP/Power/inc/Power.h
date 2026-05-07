740d22af59d8fc6a0b1637d85ae4c615053a2d28
Huang.Zihan <zihan_huang@zinovia-auto.com>
2026-05-07 15:10:50 +0800

    [APP]Power[Logic][1.实现Bat电压检测逻辑][NO ID][具体触发动作未实现]

--- APP/Power/inc/Power.h
+++ APP/Power/inc/Power.h
@@ -9,9 +9,16 @@ extern "C"
 /************************ Include Files ************************/
 
 /************************ Macro Definitions ************************/
-
+#define POWER_BAT_UNDER_VOLT           8000U
+#define POWER_BAT_OVER_VOLT            18000U
+#define POWER_BAT_AD_TO_VOLT(ad_value) ((ad_value) * 61 / 10)
 /************************ Type Definitions ************************/
-
+typedef enum
+{
+    POWER_BAT_STATUS_NORMAL,
+    POWER_BAT_STATUS_LOW_VOLT,
+    POWER_BAT_STATUS_OVER_VOLT,
+} power_bat_status_e;
 /************************ External Variables ************************/
 
 /************************ Function Declarations ************************/
--- APP/Power/src/Power.c
+++ APP/Power/src/Power.c
@@ -16,11 +16,12 @@
 #define POWER_PRINTF(...)
 #endif
 /************************ Private Global Variables ************************/
-
+static power_bat_status_e power_bat_status = POWER_BAT_STATUS_NORMAL;
 /************************ Public Global Variables ************************/
 
 /************************ Private Function Declarations ************************/
 static void _Snf_Power_Get_Response(void);
+static void _Snf_Power_Voltage_Detection(void);
 /************************ Private Function Implementations ************************/
 /**
  * @brief  Get MCU reset reason
@@ -31,6 +32,41 @@ static void _Snf_Power_Get_Response(void)
 {
     rte_mcu_reset_reason_e reset_reason = RTE_MCU_GET_RESET_REASON();
     POWER_PRINTF("Reset reason: %d\n", reset_reason);
+}
+/**
+ * @brief  Detect Bat voltage levels
+ * @param  None
+ * @return None
+ */
+static void _Snf_Power_Voltage_Detection(void)
+{
+    uint16_t           bat_ad     = RTE_ADC_GET_BAT_VOL();
+    uint16_t           bat_vol    = POWER_BAT_AD_TO_VOLT(bat_ad);
+    power_bat_status_e bat_status = POWER_BAT_STATUS_NORMAL;
+
+    if (POWER_BAT_UNDER_VOLT > bat_vol)
+    {
+        bat_status = POWER_BAT_STATUS_LOW_VOLT;
+    }
+    else if (POWER_BAT_OVER_VOLT < bat_vol)
+    {
+        bat_status = POWER_BAT_STATUS_OVER_VOLT;
+    }
+
+    if (power_bat_status != bat_status)
+    {
+        power_bat_status = bat_status;
+        POWER_PRINTF("Battery voltage: %d\r\n", bat_vol);
+
+        if (POWER_BAT_STATUS_NORMAL == power_bat_status)
+        {
+            // Handle normal voltage status
+        }
+        else
+        {
+            // Handle low voltage status
+        }
+    }
 }
 /************************ Public Function Implementations ************************/
 /**
@@ -62,5 +98,6 @@ void Snf_Power_Task_Init(void)
 void Snf_Power_Task(void)
 {
     RTE_WDG_REFRESH();
+    _Snf_Power_Voltage_Detection();
     Snf_Power_Message_Box_Handle();
 }
